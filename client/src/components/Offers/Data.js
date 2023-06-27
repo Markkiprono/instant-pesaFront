f2c5c0e4486b864ff09572479e83dc005078c3b1
Markkiprono <98320208+Markkiprono@users.noreply.github.com>
2023-06-27 22:27:02 +0300

    Create Data.js

---
+++ client/src/components/Offers/Data.js
@@ -0,0 +1,34 @@
+export const homeObjOne = {
+  id: "online",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "ONLINE CLASSES",
+  headline: "",
+  description:
+    "Online class is a paid course that we will conduct over the Internet, this is a 30day course. In online classes we will use WhatsApp, zoom, one on one calls and even sharing screens. We will conduct online classes from Monday to Friday. You can choose any day that you are comfortable with between these days. Hopefully after 30 days you will be ready to put into test what you have learn from us.",
+  imgStart: true,
+  img: require("../../asset/svg4.svg").default,
+  alt: "Online",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjTwo = {
+  id: "physical",
+  lightBg: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "PHYSICAL CLASSES",
+  headline: "",
+  description:
+    "This is one on one meetings or conference that will be held on a conference room in three major selected towns. That is Nairobi, Nakuru and Eldoret. We will be going to other cities like Kisumu and Mombasa afterwards. In the meantime, kindly select where you would wish to attend our classes from the three selected towns. Physical class is a two-day paid course that will always start from 10am to 5pm every Saturday and Sunday. After the two-day conference meeting end and you feel like you have not understood our strategy, we will always allow you to join our online classes or even our conference classes for free.",
+
+  imgStart: false,
+  img: require("../../asset/svg5.svg").default,
+  alt: "physical",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
