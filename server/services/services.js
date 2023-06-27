c0678e303c301003d05788bfb04342266ce2ab88
Markkiprono <98320208+Markkiprono@users.noreply.github.com>
2023-06-27 23:00:39 +0300

    Create services.js

---
+++ server/services/services.js
@@ -0,0 +1,17 @@
+const uri = "http://192.64.114.163";
+const passKey =
+  "f0c91c65b8ee3e344e755a77bba997c69f798dce4f525f1448d8dc7f83af53ae";
+let envt = "api";
+const ConsumerKey = "PAnO9WDlm3IFn6Mm46isl5mHBGOnGU3o";
+const consumerSecret = "WWg8VMuGl8M2JVz9";
+const shortCode = 4100363;
+const emailPassKey = "";
+module.exports = {
+  uri,
+  passKey,
+  emailPassKey,
+  envt,
+  ConsumerKey,
+  consumerSecret,
+  shortCode,
+};
