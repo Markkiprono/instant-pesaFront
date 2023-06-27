5c27cd173de37399a8ea46b03b9377d0a18526c3
Markkiprono <98320208+Markkiprono@users.noreply.github.com>
2023-06-27 22:43:52 +0300

    Create index.js

---
+++ client/src/pages/TradingAcademy/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TradingHeroSec from "../../components/TradingHeroSec";
+import TradingNav from "../../components/TradingNav";
+
+const TradingAcademy = () => {
+  return (
+    <>
+      <TradingNav />
+      <TradingHeroSec />
+    </>
+  );
+};
+
+export default TradingAcademy;
