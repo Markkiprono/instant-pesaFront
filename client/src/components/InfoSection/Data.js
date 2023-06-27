52e6cee33e2197492b76863fb4576a86cca41378
Markkiprono <98320208+Markkiprono@users.noreply.github.com>
2023-06-27 22:22:36 +0300

    Create Data.js

---
+++ client/src/components/InfoSection/Data.js
@@ -0,0 +1,38 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Android or IOS apps",
+  headline: "Use Our Apps to Manage Your Deriv Transactions",
+  description:
+    "Download our apps to manage your Deriv account on the go. You can trade, deposit, withdraw, and manage your account on your mobile device.",
+  buttonLabel: "Get Started",
+  buttonLink:
+    "https://play.google.com/store/apps/details?id=com.deriv.instantpesa",
+  imgStart: true,
+  img: require("../../asset/svg1.svg").default,
+  alt: "Netellify",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjTwo = {
+  id: "Contact",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Contact With Us",
+  headline:
+    "It's very easy to get in touch with us. Just use the contact form or call us using the numbers below.",
+  description: " Phone: 0710109009        Email : instantpesakenya1@gmail.com",
+  buttonLabel: "Contact Us",
+  buttonLink: "https://wa.me/message/T3JU2PGIY4TOL1",
+  imgStart: false,
+  img: require("../../asset/svg2.svg").default,
+  alt: "Contact",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
